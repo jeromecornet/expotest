b09b2b65ed007fdacb1a3a88150452694d189616
Jerome Cornet <jerome@jeromecornet.com>
2019-06-28 10:27:52 -0400

    Fix test

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,4 +1,4 @@
-const { reloadApp, getAppUrl } = require('detox-expo-helpers')
+const { reloadApp, getAppUrl,blacklistLiveReloadUrl,blacklistCmdlineFormat } = require('detox-expo-helpers')
 
 
 describe('Example', () => {
@@ -6,7 +6,26 @@ describe('Example', () => {
     console.log("Lauching using reloadApp")
     await reloadApp();
     console.log("App launched")
-    await expect(element(by.text('Open up App.js to start working on your app!'))).toBeVisible();
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
+  });
+
+  it('should load the app with the url', async () => {
+    console.log("Lauching expo with URL")
+    const url = await getAppUrl();
+    const blacklist = await blacklistCmdlineFormat()
+    console.log("Blacklist: ", blacklist)
+    await device.launchApp({
+      newInstance: true,
+      sourceApp: 'host.exp.exponent',
+      url,
+      launchArgs: { 
+        EXKernelDisableNuxDefaultsKey: true, 
+        detoxPrintBusyIdleResources: 'YES',
+        detoxURLBlacklistRegex: blacklist
+      },
+    })
+    console.log("App launched")
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
   });
 
  it('should open the app URL', async () => {
@@ -14,12 +33,16 @@ describe('Example', () => {
     await device.launchApp({
       newInstance: true,
       sourceApp: 'host.exp.exponent',
-      launchArgs: { EXKernelDisableNuxDefaultsKey: true},
+      launchArgs: { 
+        EXKernelDisableNuxDefaultsKey: true, 
+        detoxPrintBusyIdleResources: 'YES',
+      },
     })
+    await blacklistLiveReloadUrl()
     const url = await getAppUrl();
     console.log("Opening app URL:", url);
     await device.openURL({url})
     console.log("App launched")
-    await expect(element(by.text('Open up App.js to start working on your app!'))).toBeVisible();
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
   });
 });
